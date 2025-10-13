5bd3f86f85c3d252c182520b877ead826b414181
DIPHYL-LEIA <vanitasfinem@gmail.com>
2025-10-13 15:18:44 +0900

    기본 구조 생성

---
+++ Source/ZGP/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ZGPCharacter.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZGP_API AEnemyCharacter : public AZGPCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ZGP/ZGPPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ZGPPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZGP_API AZGPPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
